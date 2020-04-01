d1425769320f2347796c0d8e9a6d1992a693fafa
yuuta31 <odk.hello@gmail.com>
2020-04-01 18:33:32 +0900

    install_devise_except_do_UI_UX
    #what
    
    ログイン機能の実装。
    Rspecのテスト実装済み。
    デザインは未実装。

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+
+  factory :user do
+    image                 {"jjjjjjjjjjj.jpg"}
+    nickname              {"abe"}
+    email                 {"kkk@gmail.com"}
+    password              {"00000000"}
+    password_confirmation {"00000000"}
+  end
+
+end
