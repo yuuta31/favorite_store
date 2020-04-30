39b74b6e8e35170d3a3bf2345be55b9ac88cc391
yuuta31 <odk.hello@gmail.com>
2020-04-30 22:46:18 +0900

    to_clean_code
    #what
    
    リファクタリング。
    エラーハンドリング。

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,14 +1,15 @@
 class BookmarksController < ApplicationController
 
   def create
-    bookmark = current_user.bookmarks.build(post_id: params[:post_id])
-    if bookmark.save!
-      redirect_to 
-    else
-      redirect_to root_path
-    end
+    @bookmark = current_user.bookmarks.create(post_id: params[:post_id])
+    flash[:success] = "登録完了しました！"
+    redirect_to "/users/posts/#{@bookmark.post.id}"
   end
 
   def destroy
+    @bookmark = Bookmark.find_by(post_id: params[:post_id] , user_id: current_user.id)
+    @bookmark.destroy
+    flash[:success] = "登録解除しました！"
+    redirect_to "/users/posts/#{@bookmark.post.id}"
   end
 end
