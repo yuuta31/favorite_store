df6989bf41159565d80d8e5bafe17856e26d1b6c
yuuta31 <odk.hello@gmail.com>
2020-05-28 18:55:18 +0900

    to_coding_v-cloak_and_Refactoring
    #6
    
    #what
    
    ページが読み込まれるまでのマスタッシュ構文を非表示にする。
    tagのjsの機能のリファクタリング

--- app/assets/javascripts/tag.js
+++ app/assets/javascripts/tag.js
@@ -11,22 +11,5 @@ $(document).on('turbolinks:load', ()=> {
               .prev('.input').attr('checked','checked');
       }
   });
-  // ボタンをクリックしたら発動
-  $('.tag-btn').click(function() {
-
-    // もしチェックが入っていたら
-    if ($('input[name="check"]').prop('checked')) {
-  
-      // チェックを外す
-      $('input[name="check"]').prop('checked', false);
-  
-    // もしチェックが外れていたら
-    } else {
-  
-      // チェックを入れる
-      $('input[name="check"]').prop('checked', true);
-    }
-  
-  });
 });
 
